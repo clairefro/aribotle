ef7b926729697c13b3e5cf0420f505b6f0c09274
Claire Froelich <claire.froelich@gmail.com>
2023-07-22 17:01:34 -0400

    port over aribotle assets

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './Popup.css'
+import Main from "./components/Main"
 
 function App() {
-  const [crx, setCrx] = useState('create-chrome-ext')
+  // const [crx, setCrx] = useState('create-chrome-ext')
 
   return (
-    <main>
-      <h3>Popup Page!</h3>
-
-      <h6>v 0.0.0</h6>
-
-      <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">
-        Power by {crx}
-      </a>
-    </main>
+    <div class="App">
+      <Main />
+    </div>
   )
 }
 
---
+++ src/popup/components/Main.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import icon from '../../assets/logo.png';
+
+const Main = () => {
+
+  return (
+    <div>
+      <p>Hello, philosopher!</p>
+      <img src={icon} alt="extension icon" />
+
+    </div >
+  )
+}
+
+export default Main;
